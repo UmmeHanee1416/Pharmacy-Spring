e22952dfc63ac585b112f4ad8118eb35239fdb5b
UmmeHanee1416 <73064213+UmmeHanee1416@users.noreply.github.com>
2025-12-16 14:06:03 +0600

    comment postgrasql hibernate dialect

--- src/main/java/com/finalProject/FinalProject/repository/UserRepository.java
+++ src/main/java/com/finalProject/FinalProject/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.finalProject.FinalProject.repository;
 
 import com.finalProject.FinalProject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
